ac3f8098e5269c2e8e4d0b38a1edfc5c6fa08f5c
Christopher Handy <c.a.handy@hum.leidenuniv.nl>
2021-03-28 01:05:57 +0000

    better views

--- samplefiles/demoapp/views.py
+++ samplefiles/demoapp/views.py
@@ -7,10 +7,22 @@ from subprocess import Popen, PIPE, STDOUT
 
 import json
 
+from .models import Home
+from .models import Bibliography
+from .models import BibliographyEntry
+from .models import Member
+from .models import NewsItem
+from .models import Resource
+
 from .models import Dictionary
 from .models import DictionaryEntry
 
 index_template="demoapp/index.html"
+#home_template="demoapp/index.html"
+bibliography_template="demoapp/bibliography.html"
+members_template="demoapp/members.html"
+news_template="demoapp/news.html"
+resources_template="demoapp/resources.html"
 external_template="demoapp/external.html"
 search_template="demoapp/search.html"
 
@@ -19,6 +31,34 @@ commandpath="../engine/dictchk.py"
 
 def index(request):
     template = index_template
+
+    return render(request,template,{})
+
+#def home(request):
+#    template = home_template
+#
+#    return render(request,template,{})
+
+def bibliography(request):
+    template = bibliography_template
+    #bibliography = BibliographyEntry.objects.all().order_by('name')
+    bibliography = BibliographyEntry.objects.all()
+    context = { "bibliography" : bibliography, }
+
+    return render(request,template,context)
+
+def members(request):
+    template = members_template
+
+    return render(request,template,{})
+
+def news(request):
+    template = news_template
+
+    return render(request,template,{})
+
+def resources(request):
+    template = resources_template
 
     return render(request,template,{})
 
