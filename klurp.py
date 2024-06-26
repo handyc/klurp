47a3156b962d08bf45e922b43f83714dc6188ee5
Christopher Handy <handyca@mcmaster.ca>
2024-06-26 18:53:46 +0200

    Add files via upload
    a quick fix

--- klurp.py
+++ klurp.py
@@ -9,17 +9,25 @@
 import sys
 import os
 import getpass
-import glob
-import datetime
 
+import glob
 from pathlib import Path
 
+import datetime
 
-# taishostring = "/home/handyca/data/bls/"
-# dergestring = "/home/handyca/data/bls/"
+#taishostring = "/home/handyca/data/bls/"
+#dergestring = "/home/handyca/data/bls/"
+
+#text1loc = "/home/handyca/data/bls/"
+#text2loc = "/home/handyca/data/bls/"
 
 text1loc = "/Users/handyc/data/bls/"
 text2loc = "/Users/handyc/data/bls/"
+
+#text1loc = "data/bls/"
+#text2loc = "data/bls/"
+
+#/Users/handyc/data/bls
 
 # the name of this program
 friendlyname = "klurp"
@@ -31,17 +39,15 @@ defgens = 1
 defmut = 7
 
 # obtain the current time
-
-
 def now():
-    return datetime.datetime.now()
-
+    now=datetime.datetime.now()
+    return now
 
 # obtain username and current time
-user = getpass.getuser()
+user=getpass.getuser()
+timeformat="%d %b %Y %H:%M:%S"
 start_time = now()
-timeformat = "%d %b %Y %H:%M:%S"
-start_string = start_time.strftime(timeformat)
+start_string=start_time.strftime(timeformat)
 
 # obtain home directory path
 home = str(Path.home())
@@ -60,21 +66,23 @@ engine = os.path.join(klurp, "engine/")
 # the Taisho canon for Chinese and the Derge Kanjur for Tibetan
 
 
-# cleantaisho = os.path.join(klurp, "projects/openphilology/witnesses/cleaned/taisho/")
-# cleanderge = os.path.join(klurp, "projects/openphilology/witnesses/cleaned/derge/")
+#cleantaisho = os.path.join(klurp, "projects/openphilology/witnesses/cleaned/taisho/")
+#cleanderge = os.path.join(klurp, "projects/openphilology/witnesses/cleaned/derge/")
 
-# cleantaisho = os.path.join(klurp, taishostring)
-fulltext1 = os.path.join(klurp, text1loc)
+#cleantaisho = os.path.join(klurp, taishostring)
+#fulltext1 = os.path.join(klurp, text1loc)
+fulltext1 = os.path.join("/", text1loc)
 
-# cleanderge = os.path.join(klurp, dergestring)
-fulltext2 = os.path.join(klurp, text2loc)
+#cleanderge = os.path.join(klurp, dergestring)
+#fulltext2 = os.path.join(klurp, text2loc)
+fulltext2 = os.path.join("/", text2loc)
 
 # determine number of arguments passed in
-argument_count = len(sys.argv)
+argument_count=len(sys.argv)
 
 # a friendly greeting with information about start time
-print(f"Welcome to {friendlyname}, {user}!")
-print(f"{friendlyname} start: {str(start_string)}")
+print("Welcome to " + friendlyname + ", " + user + "!")
+print(friendlyname + " start: " + str(start_string))
 
 # collapse arguments to single string
 # There is a reason for using this method instead of a more
@@ -85,25 +93,27 @@ print(f"{friendlyname} start: {str(start_string)}")
 
 arguments = str(sys.argv[1:argument_count+1])
 collapsed = " ".join(sys.argv[1:])
-print(f"{str(argument_count - 1)} arguments:", collapsed, " --->")
+print(str(argument_count-1) + " arguments:", collapsed, " --->")
 
 # check for proper number of arguments (require 2 witnesses, otherwise exit)
-if argument_count > 6 and sys.argv[4].isnumeric():
-    path1 = sys.argv[1]
-    path2 = sys.argv[2]
+if argument_count>6 and sys.argv[4].isnumeric():
+#if argument_count>5 and sys.argv[4].isnumeric():
+    path1=sys.argv[1]
+    path2=sys.argv[2]
     popsize = sys.argv[3]
     generations = sys.argv[4]
     mutrate = sys.argv[5]
     dictloc = sys.argv[6]
 
+# display usage information
 else:
     print("usage:")
-    print(f"{shortname} [witness1] [witness2] [popsize] [generations] [mutation rate] [dictionary location]")
+    print(shortname + " [witness1] [witness2] [popsize] [generations] [mutation rate] [dictionary location]")
     print("")
     print("if population size is too low, evolution may not occur properly")
     print("")
     print("example:")
-    print(f"python3 {shortname} T310.31 D75 100 100 7 /Users/handyc/data/bls/example1/dictionaries")
+    print("python3 " + shortname + " T310.31 D75 100 100 7 /Users/handyc/data/bls/example1/dictionaries")
     exit()
 
 # Normalize paths (obsolete)
@@ -119,7 +129,7 @@ if len(sortedglob1) > 0:
     file1 = sortedglob1[0]
     arg1 = file1
 else:
-    print(f"Could not find a match for {globmatch1}, quitting")
+    print("Could not find a match for " + globmatch1 + ", quitting")
     exit()
 
 globmatch2 = fulltext2 + normal2 + "/*" + ".txt"
@@ -130,22 +140,20 @@ if len(sortedglob2) > 0:
     file2 = sortedglob2[0]
     arg2 = file2
 else:
-    print(f"Could not find a match for {globmatch2}, quitting")
+    print("Could not find a match for " + globmatch2 + ", quitting")
     exit()
 
 # prepare to invoke population manager
 popnum = str(popsize)
 gen = str(generations)
 mut = str(mutrate)
-fullcommand = (
-    f"{engine}population.py {gen} {popnum} {mut} {arg1} {arg2} {dictloc}"
-)
+fullcommand = engine + "population.py" + " " + gen + " " + popnum + " " + mut + " " + arg1 + " " + arg2 + " " + dictloc
 
 # remove extraneous whitespace
 fullcommand = fullcommand.strip()
 
 # inform user of fuzzy match for witness input
-# print(collapsed + " --->")
+#print(collapsed + " --->")
 print(arg1)
 print(arg2)
 print("")
@@ -167,7 +175,7 @@ total_time = end_time - start_time
 
 # inform user of start, end and total execution time
 print("")
-print(f"{friendlyname} start: {str(start_string)} end: {str(end_string)}")
-print(f"{friendlyname} total processing time: {str(total_time)}")
+print(friendlyname + " start: " + str(start_string) + " end: " + str(end_string))
+print(friendlyname + " total processing time: " + str(total_time))
 
 # This is the end of the program and nothing follows this line
